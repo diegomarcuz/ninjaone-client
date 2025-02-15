666cf00b7cab2525d16e2acc9ca3c2d566c9049b
Diego Marcuz <37422384+diegomarcuz@users.noreply.github.com>
2025-02-15 06:52:05 +0000

    refactor: schema enum

--- src/schemas/device.ts
+++ src/schemas/device.ts
@@ -1,14 +1,15 @@
+import { DeviceTypeValue } from "../constants/device"
 import { z } from "zod"
 
 export const deviceSchema = z.object({
   systemName: z.string().min(1, "System name is required").max(100, "System name must be less than 100 characters"),
-  deviceType: z.enum(["Windows workstation", "Mac workstation", "Linux workstation"], {
+  type: z.nativeEnum(DeviceTypeValue, {
     errorMap: () => ({ message: "Please select a device type" }),
   }),
   hddCapacity: z
     .string()
     .min(1, "HDD capacity must be greater than 0")
-    .max(10000, "HDD capacity must be less than 10000 GB").transform(data => Number(data)),
+    .max(10000, "HDD capacity must be less than 10000 GB")
 })
 
 export type DeviceFormSchema = z.infer<typeof deviceSchema>
